b167707340a9de1aac280b416ba005c06ffa0710
Dylan Eddies <dylan.eddies1@gmail.com>
2024-03-26 22:55:52 +0000

    Final UI iter for project

--- TimeLoggerView/ViewModels/MainViewModel.cs
+++ TimeLoggerView/ViewModels/MainViewModel.cs
@@ -2,6 +2,7 @@
 using Avalonia.Media;
 using Common.Enums;
 using DynamicData;
+using Microsoft.EntityFrameworkCore.Storage.Json;
 using Model.ModelSql;
 using ReactiveUI;
 using Service.Interface;
@@ -51,7 +52,12 @@ public class MainViewModel : ViewModelBase
 
 public class ProjectManagementViewModel : ViewModelBase
 {
-
+    private bool canModifyBudget;
+    private bool isEditing;
+    private bool isAddingAttachment;
+    private bool isBusy;
+    private string busyText = string.Empty;
+    private Project? priorState;
     private Project currentProject = new Project()
     {
         ProjectName = "Test Project Name",
@@ -66,6 +72,7 @@ Nam volutpat gravida magna, id lobortis nisl auctor ut. Curabitur ultricies feli
         Created = DateTime.Now,
         IsActive = true,
         ManhourBudget = 23,
+        ApprovalState = RequestStatus.UpdateRequested,
         Drawings = new List<Model.ModelSql.Drawing>()
         {
             new()
@@ -76,7 +83,31 @@ Nam volutpat gravida magna, id lobortis nisl auctor ut. Curabitur ultricies feli
         }
     };
 
+    public bool CanModifyBudget { get => this.canModifyBudget; set => this.RaiseAndSetIfChanged(ref this.canModifyBudget, value); }
+    public bool IsEditing
+    {
+        get => this.isEditing;
+        set
+        {
+            this.RaiseAndSetIfChanged(ref this.isEditing, value);
+            CanModifyBudget = value && (CurrentProject.ApprovalState == null
+            || CurrentProject.ApprovalState == RequestStatus.None
+            || CurrentProject.ApprovalState == RequestStatus.UpdateRequested);
+        }
+    }
+    public bool IsAddingAttachment { get => isAddingAttachment; set => this.RaiseAndSetIfChanged(ref isAddingAttachment, value); }
+    public Project? PriorState { get => this.priorState; set => this.RaiseAndSetIfChanged(ref this.priorState, value); }
     public Project CurrentProject { get => this.currentProject; set => this.RaiseAndSetIfChanged(ref this.currentProject, value); }
+
+    public ICommand BeginEditCommand { get; }
+    public ICommand SaveEditCommand { get; }
+    public ICommand CancelEditCommand { get; }
+    public ICommand SubmitBudgetCommand { get; }
+    public ICommand MarkProjectAsClosedCommand { get; }
+    public ICommand AddAttachmentCommand { get; }
+    public ICommand LogTimeCommand { get; }
+    public ICommand SubmitAttachmentCommand { get; }
+    public ICommand CancelAttachmentCommand { get; }
 
     public ObservableCollection<Project> Projects { get; set; } = new ObservableCollection<Project>()
     {
@@ -102,22 +133,30 @@ Nam volutpat gravida magna, id lobortis nisl auctor ut. Curabitur ultricies feli
 
     public ObservableCollection<Model.ModelSql.Drawing> CurrentDrawings { get; set; } = new();
 
-    public bool IsTicketApproved => CurrentProject.ApprovalState == Common.Enums.ApprovalState.Accepted;
-    public bool CanSubmit => CurrentProject.ManhourBudget > 0 && CurrentProject.ApprovalState == null;
+    private bool IsBusy { get => this.isBusy; set => this.RaiseAndSetIfChanged(ref this.isBusy, value); }
+    private string BusyText { get => this.busyText; set => this.RaiseAndSetIfChanged(ref this.busyText, value); }
+
+    public bool IsTicketApproved => CurrentProject.ApprovalState == Common.Enums.RequestStatus.Accepted;
+    public bool CanSubmit => CurrentProject.ManhourBudget > 0 && (
+        CurrentProject.ApprovalState == null
+        || CurrentProject.ApprovalState == RequestStatus.None
+        || CurrentProject.ApprovalState == RequestStatus.UpdateRequested);
 
     public string ApprovalState => CurrentProject.ApprovalState switch
     {
-        Common.Enums.ApprovalState.AwaitingApproval => "Awaiting Approval",
-        Common.Enums.ApprovalState.Accepted => nameof(Common.Enums.ApprovalState.Accepted),
-        Common.Enums.ApprovalState.Rejected => nameof(Common.Enums.ApprovalState.Rejected),
+        Common.Enums.RequestStatus.Open => "Awaiting Approval",
+        Common.Enums.RequestStatus.Accepted => nameof(Common.Enums.RequestStatus.Accepted),
+        Common.Enums.RequestStatus.Rejected => nameof(Common.Enums.RequestStatus.Rejected),
+        RequestStatus.UpdateRequested => "Update Requested",
         _ => "Not Submitted"
     };
 
     public Brush ApprovalStateBrush => CurrentProject.ApprovalState switch
     {
-        Common.Enums.ApprovalState.AwaitingApproval => new SolidColorBrush(Color.FromRgb(255, 252, 79), 1),
-        Common.Enums.ApprovalState.Accepted => new SolidColorBrush(Color.FromRgb(58, 156, 34), 1),
-        Common.Enums.ApprovalState.Rejected => new SolidColorBrush(Color.FromRgb(200, 0, 0), 1),
+        Common.Enums.RequestStatus.Open => new SolidColorBrush(Color.FromRgb(255, 252, 79), 1),
+        Common.Enums.RequestStatus.Accepted => new SolidColorBrush(Color.FromRgb(58, 156, 34), 1),
+        Common.Enums.RequestStatus.Rejected => new SolidColorBrush(Color.FromRgb(200, 0, 0), 1),
+        RequestStatus.UpdateRequested => new SolidColorBrush(Color.FromRgb(222, 133, 55), 1),
         _ => new SolidColorBrush(Color.FromRgb(170, 170, 170), 1)
     };
 
@@ -127,6 +166,15 @@ Nam volutpat gravida magna, id lobortis nisl auctor ut. Curabitur ultricies feli
     public ProjectManagementViewModel()
     {
         this.ViewProjectCommand = ReactiveCommand.Create<Project>(ViewProject);
+        this.BeginEditCommand = ReactiveCommand.Create(BeginEdit);
+        this.SaveEditCommand = ReactiveCommand.Create(SaveEdit);
+        this.CancelEditCommand = ReactiveCommand.Create(CancelEdit);
+        this.SubmitBudgetCommand = ReactiveCommand.Create(SubmitBudget);
+        this.MarkProjectAsClosedCommand = ReactiveCommand.Create(MarkProjectAsClosed);
+        this.AddAttachmentCommand = ReactiveCommand.Create(AddAttachment);
+        this.SubmitAttachmentCommand = ReactiveCommand.Create(SubmitAttachment);
+        this.CancelAttachmentCommand = ReactiveCommand.Create(CancelAttachment);
+        this.LogTimeCommand = ReactiveCommand.Create(LogTime);
     }
 
     private void ViewProject(Project project)
@@ -142,6 +190,43 @@ Nam volutpat gravida magna, id lobortis nisl auctor ut. Curabitur ultricies feli
         SukiHost.ShowDialog(App.WorkspaceInstance, projectView, allowBackgroundClose: true);
     }
 
+    private void BeginEdit()
+    {
+        this.IsEditing = true;
+        this.PriorState = (Project)CurrentProject.Clone();
+    }
+
+    private void SaveEdit() {
+        this.IsBusy = true;
+        this.BusyText = "Saving changes to project";
+    }
+    private void CancelEdit()
+    {
+        this.IsEditing = false;
+
+        if (this.PriorState != null)
+        {
+            this.CurrentProject = this.PriorState;
+        }
+    }
+    private void SubmitBudget() {
+        this.IsBusy = true;
+        this.BusyText = "Submitting budget proposal";
+    }
+    private void MarkProjectAsClosed() { }
+    private void AddAttachment()
+    {
+        this.IsAddingAttachment = true;
+    }
+    private void SubmitAttachment() {
+        this.IsBusy = true;
+        this.BusyText = "Adding Drawing";
+    }
+    private void CancelAttachment()
+    {
+        this.IsAddingAttachment = false;
+    }
+    private void LogTime() { }
 }
 
 public class TimesheetViewModel : ViewModelBase
